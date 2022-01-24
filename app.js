246a22dee78f772014e11dadac472b5694778ccc
midhun <midhunn311@gmail.com>
2022-01-24 23:34:09 +0530

    commit

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-const arrows =document.querySelectorAll(".arrow");
-const movieList =document.querySelectorAll(".movie-list");
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
 
-arrows.forEach((arrow,i)=>{
- arrow.addEventListener("click",()=>{
-    movieList[i].style.transform=`translateX(${
-        movieList[i].computedStyleMap().get("transform")[0].x.value -300}px)`;
- });
+arrows.forEach((arrow, i) => {
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  let clickCounter = 0;
+  arrow.addEventListener("click", () => {
+    const ratio = Math.floor(window.innerWidth / 270);
+    clickCounter++;
+    if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
+  });
+
+  console.log(Math.floor(window.innerWidth / 270));
+});
+
+//TOGGLE
+
+
+ball.addEventListener("click", () => {
+  items.forEach((item) => {
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
 });
