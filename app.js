c6fcecdddc6a383d3993ef2ad16dc1f41f6427ea
midhun <midhunn311@gmail.com>
2022-01-24 23:35:26 +0530

    commit

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ arrows.forEach((arrow, i) => {
 
 //TOGGLE
 
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
+);
 
 ball.addEventListener("click", () => {
   items.forEach((item) => {
